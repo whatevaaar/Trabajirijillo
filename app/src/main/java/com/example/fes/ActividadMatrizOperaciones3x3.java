068f863b38776153324666ff049291bb3e169c84
Whatevaaar <emilianohidalgo314@gmail.com>
2019-05-21 20:29:26 -0500

    Matrices v1

--- app/src/main/java/com/example/fes/ActividadMatrizOperaciones3x3.java
+++ app/src/main/java/com/example/fes/ActividadMatrizOperaciones3x3.java
@@ -29,9 +29,13 @@ public class ActividadMatrizOperaciones3x3 extends AppCompatActivity {
             Button botonCalcular = (Button) findViewById(R.id.botonCalcular3x3);
          inputCelda1x1 = (EditText) findViewById(R.id.celda1x1);
          inputCelda1x2 = (EditText) findViewById(R.id.celda1x2);
-         inputCelda2x1 = (EditText) findViewById(R.id.celda2x1);
+         inputCelda1x3 = (EditText) findViewById(R.id.celda2x1);
+         inputCelda2x1 = (EditText) findViewById(R.id.celda2x2);
          inputCelda2x2 = (EditText) findViewById(R.id.celda2x2);
-
+         inputCelda2x3 = (EditText) findViewById(R.id.celda2x2);
+         inputCelda3x1 = (EditText) findViewById(R.id.celda2x2);
+         inputCelda3x2 = (EditText) findViewById(R.id.celda2x2);
+         inputCelda3x3 = (EditText) findViewById(R.id.celda2x2);
          textMatrizTraspuesta = (TextView) findViewById(R.id.matrizTraspuesta);
          textTraza= (TextView) findViewById(R.id.traza);
          textMatrizInversa= (TextView) findViewById(R.id.matrizInversa);
@@ -48,34 +52,71 @@ public class ActividadMatrizOperaciones3x3 extends AppCompatActivity {
 
         try {
             int celda1x1 = Integer.parseInt(inputCelda1x1.getText().toString()); //gets you the contents of edit text
-            int celda1x2 = Integer.parseInt(inputCelda1x1.getText().toString()); //gets you the contents of edit text
-            int celda2x1 = Integer.parseInt(inputCelda1x1.getText().toString()); //gets you the contents of edit text
-            int celda2x2 = Integer.parseInt(inputCelda1x1.getText().toString()); //gets you the contents of edit text
-            int determinante = calcularDeterminante(celda1x1,celda2x2, celda1x2, celda2x1);
-            textTraza.setText("Traza: " + calcularTraza(celda1x1,celda2x2));
-            textMatrizInversa.setText(calcularInversa(celda1x1, celda1x2, celda2x1,celda2x2, determinante));
-            textMatrizTraspuesta.setText(calcularTraspuesta(celda1x1,celda1x2, celda2x1, celda2x2));
+            int celda1x2 = Integer.parseInt(inputCelda1x2.getText().toString()); //gets you the contents of edit text
+            int celda1x3 = Integer.parseInt(inputCelda1x3.getText().toString()); //gets you the contents of edit text
+            int celda2x1 = Integer.parseInt(inputCelda2x1.getText().toString()); //gets you the contents of edit text
+            int celda2x2 = Integer.parseInt(inputCelda2x1.getText().toString()); //gets you the contents of edit text
+            int celda2x3 = Integer.parseInt(inputCelda2x1.getText().toString()); //gets you the contents of edit text
+            int celda3x1 = Integer.parseInt(inputCelda3x1.getText().toString()); //gets you the contents of edit text
+            int celda3x2 = Integer.parseInt(inputCelda3x2.getText().toString()); //gets you the contents of edit text
+            int celda3x3 = Integer.parseInt(inputCelda3x3.getText().toString()); //gets you the contents of edit text
+            int determinante = calcularDeterminante(celda1x1,celda1x2, celda1x3,
+                                                            celda2x1,celda2x2, celda2x3,
+                                                            celda3x1,celda3x2, celda3x3);
+            textTraza.setText("Traza: " + calcularTraza(celda1x1,celda2x2,celda3x3));
+            textMatrizInversa.setText(calcularInversa(celda1x1,celda1x2, celda1x3,
+                                                            celda2x1,celda2x2, celda2x3,
+                                                            celda3x1,celda3x2, celda3x3, determinante));
+            textMatrizTraspuesta.setText(calcularTraspuesta(celda1x1,celda1x2, celda1x3,
+                                                            celda2x1,celda2x2, celda2x3,
+                                                            celda3x1,celda3x2, celda3x3));
             textDeterminante.setText("Determinante: " + determinante);
         }catch (Exception e){
             return;
         }
     }
 
-    protected String calcularTraspuesta(int celda1x1, int celda1x2, int celda2x1, int celda2x2){
-        return celda1x1 + "\t" + celda2x1 + "\n" + celda1x2 + "\t" + celda2x2;
+    protected String calcularTraspuesta(int celda1x1, int celda1x2, int celda1x3, int celda2x1,int celda2x2,
+                                        int celda2x3, int celda3x1, int celda3x2, int celda3x3){
+        return celda1x1 + "\t" + celda2x1 +  "\t" + celda3x1 + "\n" +
+                celda1x2 + "\t" + celda2x2+  "\t" + celda3x2 + "\n" +
+                celda1x3 + "\t" + celda2x3+  "\t" + celda3x3;
     }
 
-    protected int calcularTraza(int celda1x1, int celda2x2){
-        return celda1x1 + celda2x2;
+    protected int calcularTraza(int celda1x1, int celda2x2, int celda3x3){
+        return celda1x1 + celda2x2 + celda3x3;
     }
 
-    protected String calcularInversa(int celda1x1, int celda1x2, int celda2x1, int celda2x2, int determinante){
+    protected String calcularInversa(int celda1x1,int celda1x2, int celda1x3,
+                                                            int celda2x1,int celda2x2, int celda2x3,
+                                                            int celda3x1,int celda3x2, int celda3x3, int determinante){
        int multiplicador = 1/ determinante;
-       return (celda2x2 * multiplicador) + "\t" + (celda1x2 * -1 * multiplicador) + "\n" + (celda2x1 * -1 * multiplicador) + "\t" + (celda1x1 * multiplicador);
+       int termino1x1 =multiplicador * calcularCoofactor(celda2x2,celda2x3,celda3x2,celda3x3);
+       int termino1x2= -1 * multiplicador * calcularCoofactor(celda1x2,celda1x3,celda2x2,celda2x3);
+       int termino1x3= multiplicador * calcularCoofactor(celda1x2,celda1x3,celda2x2,celda2x3);
+       int termino2x1= -1 *multiplicador * calcularCoofactor(celda2x1,celda2x3,celda3x1,celda3x3);
+       int termino2x2= multiplicador * calcularCoofactor(celda1x1,celda1x3,celda3x1,celda3x3);
+       int termino2x3= -1 * multiplicador * calcularCoofactor(celda1x1,celda1x3,celda2x1,celda2x3);
+       int termino3x1= multiplicador * calcularCoofactor(celda2x1,celda2x2,celda3x1,celda3x2);
+       int termino3x2= -1 * multiplicador * calcularCoofactor(celda1x1,celda1x2,celda3x1,celda3x2);
+       int termino3x3= multiplicador * calcularCoofactor(celda1x1,celda1x2,celda2x1,celda2x2);
+       return termino1x1 + "\t" + termino2x1 +  "\t" + termino3x1 + "\n" +
+                termino1x2 + "\t" + termino2x2+  "\t" + termino3x2 + "\n" +
+                termino1x3 + "\t" + termino2x3+  "\t" + termino3x3;
     }
 
-    protected int calcularDeterminante(int celda1x1, int celda2x2, int celda2x1, int celda1x2) {
-        return celda1x1 * celda2x2 - celda1x2 * celda2x1;
+    protected int calcularCoofactor(int a0,  int a1, int b0, int b1){
+        return a0 * b1 - b0 * a1;
+    }
+
+    protected int calcularDeterminante(int celda1x1, int celda1x2, int celda1x3, int celda2x1,int celda2x2,
+                                        int celda2x3, int celda3x1, int celda3x2, int celda3x3) {
+        return (celda1x1 * celda2x2 * celda3x3 +
+                celda2x1 * celda3x2 * celda1x1 +
+                celda3x1 * celda1x2 * celda2x3) -
+                (celda3x3 * celda2x1 * celda1x2 +
+                celda2x3 * celda3x2 * celda1x1 +
+                celda1x3 * celda2x2 * celda3x1);
     }
 
 }
