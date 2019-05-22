306b1d14fd173c56ee70579ec5a37ede4ba8de42
Curi Arcos <carlos.abarca.14@gmail.com>
2019-05-21 19:27:22 -0500

    se agregan actividades

--- app/src/main/java/com/example/fes/MenuActivity.java
+++ app/src/main/java/com/example/fes/MenuActivity.java
@@ -1,24 +1,26 @@
 package com.example.fes;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
     private Button btIniciar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        btIniciar = (Button) findViewById(R.id.buttonIniciar);
+        setContentView(R.layout.activity_menu);
+
+        btIniciar = (Button) findViewById(R.id.buttonEcLineales);
         btIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+                Intent intent = new Intent(MenuActivity.this,EcuActivity.class);
 
                 startActivity(intent);
             }
